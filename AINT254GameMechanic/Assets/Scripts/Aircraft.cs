7d97f30d17d09293bee552ee5aa2816b7bfe74c2
lwest2 <liam.west@students.plymouth.ac.uk>
2017-10-17 17:13:09 +0100

    Fixing the flight
    + Added correct torque
    + Added constant velocity
    / commented a bunch of code for later development, all inputs need to be placed in update()

--- AINT254GameMechanic/Assets/Scripts/Aircraft.cs
+++ AINT254GameMechanic/Assets/Scripts/Aircraft.cs
@@ -9,9 +9,15 @@ public class Aircraft : MonoBehaviour {
 
     private float currentSpeed;
     private float applySpeed = 1000.0f;
-    private float minAltitudeSpeed = 150.0f;
+    private float minAltitudeSpeed = 50.0f;
     private float maxSpeed = 250.0f;
     private float breakSpeed = 500.0f;
+
+    [SerializeField]
+    private float torque = 50.0f;
+
+    private bool flyingSpeed = true;
+    private float inputValue;
 
     // Use this for initialization
     void Start () {
@@ -19,34 +25,44 @@ public class Aircraft : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-        currentSpeed = rb.velocity.magnitude;
-
-        
-
-        if (Input.GetKey(KeyCode.W))
+	void FixedUpdate ()
+    {
+        if (flyingSpeed)
         {
-            if (currentSpeed >= maxSpeed)
-            {
-                currentSpeed = maxSpeed;
-                Debug.Log("Staying at max speed");
-            }
-            else
-            {
-                rb.AddRelativeForce(Vector3.forward * applySpeed * Time.deltaTime * 0.5f, ForceMode.Acceleration);
-                Debug.Log(rb.velocity.magnitude);
-            }
-            if (currentSpeed >= minAltitudeSpeed)
-            {
-                
-                Debug.Log("Fly");
-            }
+            rb.AddTorque(transform.right * torque * inputValue);
+            rb.velocity = transform.forward * 5;
         }
+        //currentSpeed = rb.velocity.magnitude;
 
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddRelativeForce(Vector3.back * breakSpeed * Time.deltaTime, ForceMode.Acceleration);
-        }
-        
-	}
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    if (currentSpeed >= maxSpeed)
+        //    {
+        //        currentSpeed = maxSpeed;
+        //        Debug.Log("Staying at max speed");
+        //    }
+        //    else
+        //    {
+        //        rb.AddRelativeForce(transform.forward * applySpeed * Time.deltaTime * 0.5f, ForceMode.Acceleration);
+        //        Debug.Log(rb.velocity.magnitude);
+        //    }
+
+        //    if (currentSpeed >= minAltitudeSpeed)
+        //    {                
+        //        rb.AddRelativeTorque(transform.right * torque);
+        //        Debug.Log("Fly");
+        //    }
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    rb.AddRelativeForce(-transform.forward * breakSpeed * Time.deltaTime, ForceMode.Acceleration);
+        //}
+
+    }
+
+    private void Update()
+    {
+        inputValue = Input.GetAxis("Vertical");
+    }
 }
