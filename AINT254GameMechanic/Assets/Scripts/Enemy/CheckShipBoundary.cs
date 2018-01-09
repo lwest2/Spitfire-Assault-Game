198103d528795292e3c4deef5a91a4fc906f9991
lwest <liam.west@students.plymouth.ac.uk>
2018-01-09 13:36:17 +0000

    Boundary, GUI
    + Updated GUI, with buttons and titles
    + Ship movement and boundary adjusted accordingly so that the ships would retain a small paremeter while still keeping their random motion between the inner and outter colliders.

---
+++ AINT254GameMechanic/Assets/Scripts/Enemy/CheckShipBoundary.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aircraft
+{
+    public class CheckShipBoundary : MonoBehaviour
+    {
+
+        private bool outsideBoundary = false;
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "ShipBoundary")
+            {
+                outsideBoundary = false;
+                Debug.Log("IsNowOutsideBoundary");
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "innerShipBoundary")
+            {
+                outsideBoundary = true;
+                Debug.Log("IsNowInsideBoundary");
+            }
+        }
+
+        public bool getOutsideBoundary()
+        {
+            return outsideBoundary;
+        }
+    }
+}
--- AINT254GameMechanic/Assets/Scripts/Enemy/ShipMovement.cs
+++ AINT254GameMechanic/Assets/Scripts/Enemy/ShipMovement.cs
@@ -2,52 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipMovement : MonoBehaviour {
-
-    // references - http://wiki.unity3d.com/index.php/Wander
-
-    private float m_speed = 10f;
-    private float m_directionChange = 10f;
-    private float m_timeToDirect = 0.2f;
-    private float m_maxHeadingChange = 30;
-    private float m_heading;
-    Vector3 m_targetRotation;
-    private Rigidbody m_rb;
-
-    private void Awake()
+namespace Aircraft
+{
+    public class ShipMovement : MonoBehaviour
     {
-        m_heading = Random.Range(0, 360);
-        transform.eulerAngles = new Vector3(0, m_heading, 0);
 
-        StartCoroutine(NewHeading());
-    }
-    // Use this for initialization
-    void Start () {
-        m_rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, m_targetRotation, Time.deltaTime * m_timeToDirect);
-        m_rb.velocity = transform.forward * m_speed;
-	}
+        // references - http://wiki.unity3d.com/index.php/Wander
 
-    // calculates new direction to move towards
-    IEnumerator NewHeading()
-    {
-        while(true)
+        private float m_speed = 10f;
+        private float m_directionChange = 10f;
+        private float m_timeToDirect = 0.2f;
+        private float m_maxHeadingChange = 30;
+        private float m_heading;
+        Vector3 m_targetRotation;
+
+        private Rigidbody m_rb;
+
+        private Transform shipBoundaryCenter;
+
+        private CheckShipBoundary m_checkShipBoundary_script;
+
+        private Vector3 m_dir;
+        private Quaternion m_lookRot;
+
+        private bool m_outsideBoundary;
+
+        private void Awake()
         {
-            NewHeadingDirection();
-            yield return new WaitForSeconds(m_directionChange);
-        }
-    }
 
-    // calculates new direction
-    void NewHeadingDirection()
-    {
-        float floor = Mathf.Clamp(m_heading - m_maxHeadingChange, 0, 360);
-        float ceil = Mathf.Clamp(m_heading + m_maxHeadingChange, 0, 360);
-        m_heading = Random.Range(floor, ceil);
-        m_targetRotation = new Vector3(0, m_heading, 0);
+            m_checkShipBoundary_script = GetComponent<CheckShipBoundary>();
+
+            m_heading = Random.Range(0, 360);
+            transform.eulerAngles = new Vector3(0, m_heading, 0);
+
+            StartCoroutine(NewHeading());
+        }
+        // Use this for initialization
+        void Start()
+        {
+            m_rb = GetComponent<Rigidbody>();
+            shipBoundaryCenter = GameObject.FindGameObjectWithTag("ShipBoundary").GetComponent<Transform>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            m_outsideBoundary = m_checkShipBoundary_script.getOutsideBoundary();
+            Debug.Log(m_outsideBoundary);
+            if (m_outsideBoundary)
+            {
+                transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, m_targetRotation, Time.deltaTime * m_timeToDirect / 10);
+                Debug.Log("NoSpecificDirection");
+            }
+            else
+            {
+                m_dir = shipBoundaryCenter.position - transform.position; // offset from player
+
+                m_dir.y = 0;
+
+                m_lookRot = Quaternion.LookRotation(m_dir);
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, m_lookRot, Time.deltaTime * m_timeToDirect / 10);
+
+
+                Debug.Log("FacingCenter");
+            }
+
+            m_rb.velocity = transform.forward * m_speed;
+        }
+
+        // calculates new direction to move towards
+        IEnumerator NewHeading()
+        {
+            while (true)
+            {
+                
+                    NewHeadingDirection();
+                    yield return new WaitForSeconds(m_directionChange);
+                
+            }
+        }
+
+        // calculates new direction
+        void NewHeadingDirection()
+        {
+            if (m_outsideBoundary)
+            {
+                float floor = Mathf.Clamp(m_heading - m_maxHeadingChange, 0, 360);
+                float ceil = Mathf.Clamp(m_heading + m_maxHeadingChange, 0, 360);
+                m_heading = Random.Range(floor, ceil);
+                Debug.Log(m_heading);
+                m_targetRotation = new Vector3(0, m_heading, 0);
+            }
+            
+            
+        }
+
     }
 }
